0dd39d1a0e4e811969f769d4d52caa2f494e0187
Matheus Moya de Oliveira <167132031+matheusmoyaoliveira@users.noreply.github.com>
2024-05-28 18:18:49 -0300

    Exercicios da aula 09

---
+++ pythonteste/aula09.py
@@ -0,0 +1,8 @@
+frase = 'Curso em Video Python'
+print(frase[::2])
+
+print('Oi')
+
+print("""Lorem ipsum dolor sit amet consectetur adipisicing elit.
+Qui, temporibus. Nostrum, fugiat ullam quidem qui blanditiis mollitia ab architecto
+praesentium quisquam esse quod nam consectetur, voluptatum, non libero suscipit? Quo!""")
