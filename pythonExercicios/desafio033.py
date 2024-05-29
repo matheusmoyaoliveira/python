b3519c5b0e3f50070d8cac8927fbda5db3059f47
Matheus Moya de Oliveira <167132031+matheusmoyaoliveira@users.noreply.github.com>
2024-05-29 20:58:44 -0300

    Término do mundo 1

---
+++ pythonExercicios/desafio033.py
@@ -0,0 +1,17 @@
+a = int(input('Primeiro número: '))
+b = int(input('Segundo número: '))
+c = int(input('Terceiro número: '))
+# verificando quem é menor
+menor = a
+if b < a and b < c:
+    menor = b
+if c < a and c < b:
+    menor = c
+# Verificando quem é o maior
+maior = a
+if b > a and b > c:
+    maior = b
+if c > a and c > b:
+    maior = c
+print('O menor valor digitado foi {}'.format(menor))
+print('O maior valor digitado foi {}'.format(maior))
