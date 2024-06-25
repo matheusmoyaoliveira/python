60f943651fb7f9fa87c6823f826d882936680471
Matheus Moya de Oliveira <167132031+matheusmoyaoliveira@users.noreply.github.com>
2024-06-25 03:49:46 -0300

    aula 13

---
+++ pythonExercicios/desafio046.py
@@ -0,0 +1,5 @@
+from time import sleep
+for cont in range(10, -1, -1):
+    sleep(1)
+    print(cont)
+print('BUM! BUM! POOOW!')
---
+++ pythonteste/aula13.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um número: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+for c in range(i, f+1, p):
+    print(c)
+print('FIM')
