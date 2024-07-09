4406e04a40cd0d3a8ee8b72859c236d9a63ae1c9
Matheus Moya de Oliveira <167132031+matheusmoyaoliveira@users.noreply.github.com>
2024-07-08 21:06:02 -0300

    Inicio da aula 14 e exercicios

---
+++ pythonExercicios/desafio059.py
@@ -0,0 +1,34 @@
+from time import sleep
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
+opçao = 0
+while opçao != 5:
+    print('''    [ 1 ] somar
+    [ 2 ] multiplicar
+    [ 3 ] maior
+    [ 4 ] novos números
+    [ 5 ] sair do programa''')
+    opçao = int(input('>>>>>> Qual é a sua opção? '))
+    if opçao == 1:
+        soma = n1 + n2
+        print('A soma entre {} + {} é {}'.format(n1, n2, soma))
+    elif opçao == 2:
+        produto = n1 * n2
+        print('O resultado de {} X {} é {}'.format(n1, n2, produto))
+    elif opçao == 3:
+        if n1 > n2:
+            maior = n1
+        else:
+            maior = n2
+        print('Entre {} e {} o maior valor é {}'.format(n1, n2, maior))
+    elif opçao == 4:
+        print('Informe os números novamente: ')
+        n1 = int(input('Primeiro valor: '))
+        n2 = int(input('Segundo valor: '))
+    elif opçao == 5:
+        print('Finalizando...')
+    else:
+        print('Opção inválida. Tente novamente')
+    print('=-=' * 10)
+    sleep(2)
+print('Fim do programa! Volte sempre!')
