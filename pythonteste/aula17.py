9979134c8a4accc13e07cc78a13d4e4ac699b366
Matheus Moya de Oliveira <167132031+matheusmoyaoliveira@users.noreply.github.com>
2024-07-22 21:01:21 -0300

    exercicios da aula 17

---
+++ pythonteste/aula17.py
@@ -0,0 +1,11 @@
+num = [2, 5, 9, 1]
+num[2] = 3
+num.append(7)
+num.sort(reverse=True)
+num.insert(2, 2)
+if 5 in num:
+    num.remove(5)
+else:
+    print('Não achei o número 5')
+print(num)
+print(f'Essa lista tem {len(num)} elementos.')
