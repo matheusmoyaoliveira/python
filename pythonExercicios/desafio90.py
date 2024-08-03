59f7a8a00f4890b3cc5088f71760172c35331671
Matheus Moya de Oliveira <167132031+matheusmoyaoliveira@users.noreply.github.com>
2024-08-03 02:30:37 -0300

    Exercicios

---
+++ pythonExercicios/desafio90.py
@@ -0,0 +1,12 @@
+aluno = dict()
+aluno['nome'] = str(input('Nome: '))
+aluno['media'] = float(input(f'Média de {aluno["nome"]}: '))
+if aluno['media'] >= 7:
+    aluno['situaçao'] = 'Aprovado'
+elif 5 <= aluno['media'] < 7:
+    aluno['situaçao'] = 'Recuperação'
+else:
+    aluno['situaçao'] = 'Reprovado'
+print('-=' * 30)
+for k, v in aluno.items():
+    print(f'  - {k} é igual a {v}')
